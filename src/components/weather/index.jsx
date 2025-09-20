722a191766c77bc53f9eb76f97820512bedaf95e
Talupula Naveen <stylistar05@gmail.com>
2025-09-20 10:55:37 +0530

    Use API for Dynamic data

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,9 +1,43 @@
+import { useState, useEffect } from "react";
 import SearchBar from "../search";
 
 export default function Weather() {
+  const [info, setInfo] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=e34b4c51d8c2b7bf48d5217fe52ff79e`;
+
+  async function fetchUrl(comingUrl) {
+    setLoading(true);
+    setError(false);
+    try {
+      const response = await fetch(comingUrl);
+      const data = await response.json();
+      setInfo(data);
+      setLoading(false);
+      setError(false);
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchUrl(url);
+  }, [url]);
+
+  if (loading) {
+    return <div>Loading pls wait....</div>;
+  }
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <div className="app-container">
-      <div className="weather-card">
+      {/* <div className="weather-card">
         <SearchBar />
 
         <div className="weather-info">
@@ -23,7 +57,7 @@ export default function Weather() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
